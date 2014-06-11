e1a284d3aca616bde62d2bb21f6fa136fbde451a
Jörg Thalheim <joerg@higgsboson.tk>
2014-06-12 00:44:37 +0200

    game.h: include seq_file

--- game.c
+++ game.c
@@ -6,7 +6,6 @@
 #include <linux/kernel.h>
 #include <asm-generic/uaccess.h>
 #include <linux/random.h>
-#include <linux/seq_file.h>
 #include "game.h"
 
 static u32 score = 0;
--- game.h
+++ game.h
@@ -1,11 +1,7 @@
-
+#include <linux/seq_file.h>
 #define BOARD_SIZE (4)
 
 void draw_board(struct seq_file *f, u16 board[BOARD_SIZE][BOARD_SIZE]);
-
 bool game_ended(u16 board[BOARD_SIZE][BOARD_SIZE]);
-
 void handle_key(u16 board[BOARD_SIZE][BOARD_SIZE], char key);
-
 void reset_game(u16 board[BOARD_SIZE][BOARD_SIZE]);
-
